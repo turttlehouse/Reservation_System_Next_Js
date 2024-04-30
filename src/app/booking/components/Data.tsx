c5c8bc68517f1905cca0e7dbe1a59dbe2af2bf63
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-04-30 13:53:46 +0545

    rates finsihed

--- src/app/booking/components/Data.tsx
+++ src/app/booking/components/Data.tsx
@@ -7,7 +7,7 @@ export default function Data(){
           <h1 className="text-black ml-5">#555</h1>
           <h1 className="text-black ml-[72px]">Ghanashyam</h1>
           <h1 className="text-black">52(Terrace)</h1>
-          <h1 className="text-black ml-3">19/03/2024 <br/> 9:00 Am</h1>
+          <h1 className="text-black ml-3">19/03/2024 <br/> 9:00 AM</h1>
           <h1 className="text-black ml-6">5</h1>
           <h1 className="text-black ml-[100px]">$1500</h1>
           <h1 className="text-black ml-[50px]">$1500</h1>
---
+++ src/app/rates/components/Data.tsx
@@ -0,0 +1,18 @@
+export default function Data(){
+  return(
+    <>
+    <div className="flex ml-[390px]">
+    <div className="bg-[#f4f4f9] h-[40px] rounded-md flex items-center mt-3">
+      <div className="flex gap-5 ">
+          <h1 className="text-black ml-5">#5124</h1>
+          <h1 className="text-black ml-10">Laliguras Sofa</h1>
+          <h1 className="text-black ml-5">Terrace Hall</h1>
+          <h1 className="text-black ml-16">$1500</h1>
+          <h1 className="text-black ml-12">$1000</h1>
+          <h1 className="text-black ml-8">2 Persons</h1>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
