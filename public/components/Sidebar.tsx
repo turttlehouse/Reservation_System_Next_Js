9d422962ff8ddcdcd75f1952b626a4bcbc032a2e
Rohan-chy <chyrohan036@gmail.com>
2024-04-29 17:49:51 -0700

    Revert "frontdesk logo and sidebar width change"
    This reverts commit c4b531d06d538690f5a4ab66e0fc19f4493d972e.

--- public/components/Sidebar.tsx
+++ public/components/Sidebar.tsx
@@ -10,39 +10,39 @@ import logout from '../images/logout.png'
 
 const Sidebar = () => {
   return (
-      <div className="w-[200px] h-screen ">
-        <ul className="flex flex-col gap-5 ">
-          <li className="flex items-center gap-2 cursor-pointer">
+      <div className="w-[300px] h-screen ">
+        <ul className="flex flex-col gap-5">
+          <li className="flex items-center gap-2">
             <Image src={home} alt="dashboard" width={20} height={20}/>
             <span>Dashboard</span>
           </li>
-          <li className="flex items-center gap-2 cursor-pointer">
+          <li className="flex items-center gap-2">
             <Image src={front} alt="dashboard" width={20} height={20}/>
             <span>Front Desk</span>
           </li>
-          <li className="flex items-center gap-2 cursor-pointer">
+          <li className="flex items-center gap-2">
             <Image src={bookings} alt="dashboard" width={20} height={20}/>
             <span>Bookings</span>
           </li>
-          <li className="flex items-center gap-2 cursor-pointer">
+          <li className="flex items-center gap-2">
             <Image src={tables} alt="dashboard" width={20} height={20}/>
             <span>Tables</span>
           </li>
-          <li className="flex items-center gap-2 cursor-pointer">
+          <li className="flex items-center gap-2">
             <Image src={deals} alt="dashboard" width={20} height={20}/>
             <span>Deals</span>
           </li>
-          <li className="flex items-center gap-2 cursor-pointer">
+          <li className="flex items-center gap-2">
             <Image src={rates} alt="dashboard" width={20} height={20}/>
             <span>Rates</span>
           </li>
-          <li className="flex items-center gap-2 cursor-pointer">
+          <li className="flex items-center gap-2">
             <Image src={vendor} alt="dashboard" width={20} height={20}/>
             <span>Vendor</span>
           </li>
           
         </ul>
-        <div className="flex items-center gap-2 mt-[50px] cursor-pointer">
+        <div className="flex items-center gap-2 mt-[50px]">
           <Image src={logout} alt="" width={20} height={20}/>
           <span>Logout</span>
         </div>
